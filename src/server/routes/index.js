3664eb3fcaab5233e7eb6a7db3dc979bf631337a
Viatcheslav Ivanov <slava.eth@gmail.com>
2016-08-23 23:59:05 -0700

    feat: api keys (#3)

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 
-import fuelPurchases from './fuelPurchases.route'
 import auth from './auth.route'
+import fuelPurchases from './fuelPurchases.route'
+import apiKeys from './apiKeys.route'
 
 const router = new Router()
 
@@ -9,7 +10,8 @@ router.get('/', (req, res) =>
 	res.json()
 )
 
-router.use('/fuelPurchases', fuelPurchases)
 router.use('/auth', auth)
+router.use('/fuelPurchases', fuelPurchases)
+router.use('/apiKeys', apiKeys)
 
 export default router
